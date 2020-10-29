7fc79a1825117d56f3607e31c976ee2964769366
Kathleen Dorse <kathleen.dorse@gmail.com>
2020-10-28 19:05:41 -0500

    deploying

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
